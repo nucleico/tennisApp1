7908a9dec324eed484a74024b477eadf158ed4f6
Guido Valenzuela <nucleico@gmail.com>
2020-02-07 02:29:42 -0300

    Updates

---
+++ precache-manifest.89c0b3b1677c492bd5eb2defc710962c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da6335f7f9b8da8efcd0db28fc4afef",
+    "url": "/tennisApp1/index.html"
+  },
+  {
+    "revision": "1496dff22034c70b7a16",
+    "url": "/tennisApp1/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b909a5246eea6829147b",
+    "url": "/tennisApp1/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "1496dff22034c70b7a16",
+    "url": "/tennisApp1/static/js/2.8ec5975b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tennisApp1/static/js/2.8ec5975b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b909a5246eea6829147b",
+    "url": "/tennisApp1/static/js/main.e0150fc8.chunk.js"
+  },
+  {
+    "revision": "1ad3b3d7291b2fb00a4b",
+    "url": "/tennisApp1/static/js/runtime-main.0b8a2810.js"
+  },
+  {
+    "revision": "f4a81a7875c4a4fbac95f0256c751956",
+    "url": "/tennisApp1/static/media/am.f4a81a78.jpeg"
+  },
+  {
+    "revision": "df17bc388e7967a12178b70f1c38a66a",
+    "url": "/tennisApp1/static/media/az.df17bc38.jpg"
+  },
+  {
+    "revision": "50f256ca8bc683b0de5ffefd25205531",
+    "url": "/tennisApp1/static/media/dt.50f256ca.jpg"
+  },
+  {
+    "revision": "7c53ccdd9a2c6cad142a0101112540c3",
+    "url": "/tennisApp1/static/media/gm.7c53ccdd.jpeg"
+  },
+  {
+    "revision": "fd6d614fe0cee1db7b53b2a9e0559ce6",
+    "url": "/tennisApp1/static/media/jd.fd6d614f.jpeg"
+  },
+  {
+    "revision": "f373c5090bb15873bf5f1d8eefef70bd",
+    "url": "/tennisApp1/static/media/nd.f373c509.jpeg"
+  },
+  {
+    "revision": "7ecd38116289c09c2bb39bed65e418d4",
+    "url": "/tennisApp1/static/media/rf.7ecd3811.jpg"
+  },
+  {
+    "revision": "e47725409fb39e16b58d600dcfbbc0c7",
+    "url": "/tennisApp1/static/media/rn.e4772540.jpeg"
+  },
+  {
+    "revision": "d1d202cfe58a405ccc15111bb1a57a37",
+    "url": "/tennisApp1/static/media/st.d1d202cf.jpg"
+  }
+]);
